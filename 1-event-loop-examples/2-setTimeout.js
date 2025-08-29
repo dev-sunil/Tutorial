ed87d7ebc7992b126010ad413e733d77023ca7c8
Sunil Samantaray <sunilnine3@gmail.com>
2025-08-29 19:24:28 +0530

    Few codes added

---
+++ 1-event-loop-examples/2-setTimeout.js
@@ -0,0 +1,8 @@
+//Started operating system process
+console.log('First');
+setTimeout(() => {
+    console.log('Second');
+}, 0);
+
+console.log('Third');
+//Completed and exited operating system process
---
+++ 1-event-loop-examples/3-setInterval.js
@@ -0,0 +1,5 @@
+//Started operating system process
+setInterval(() => {
+    console.log('Hello world!');
+}, 2000);
+console.log('I will run first');
